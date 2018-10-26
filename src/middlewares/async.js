94ba4037e7c2d1e96a391d6c029763553d5f0b79
Scott Fox <scottfox87@gmail.com>
2018-10-26 10:58:17 -0500

    dispatch the new action with response as payload

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,6 +1,11 @@
 export default ({ dispatch }) => next => action => {
     if (!action.payload || !action.payload.then ) {
         return next(action);
+    } else {
+        action.payload.then(function(response) {
+            const newAction = { ...action, payload: response };
+            dispatch(newAction); //takes the action and sends through all the middlewares again
+        });
     }
 }
 
